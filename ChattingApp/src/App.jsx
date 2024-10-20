b7282d7621ec65379fde17a66c78c65988d3c77a
Filip Elznic <elznicfilip@gmail.com>
2024-10-20 21:10:51 +0200

    SignIn

--- ChattingApp/src/App.jsx
+++ ChattingApp/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { collection, query, orderBy, limit } from "firebase/firestore";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqets2wa5qliHWXxKe0Vo2lCclkq3n7-U",
+  authDomain: "chattingapp-filip.firebaseapp.com",
+  projectId: "chattingapp-filip",
+  storageBucket: "chattingapp-filip.appspot.com",
+  messagingSenderId: "334026627618",
+  appId: "1:334026627618:web:e8fb1790b33b808c31887b",
+  measurementId: "G-51P0TGJWCZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => signOut(auth)}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const messagesRef = collection(firestore, "messages");
+  const messagesQuery = query(messagesRef, orderBy("createdAt"), limit(25));
+
+  const [messages] = useCollectionData(messagesQuery, { idField: "id" });
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <SignOut />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+  return <p>{text}</p>;
+}
+
+export default App;

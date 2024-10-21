e8bb99a8c1e16503395adfb81ecb650818cd2b85
Filip Elznic <elznicfilip@gmail.com>
2024-10-21 09:51:55 +0200

    routing for better styling

---
+++ ChattingApp/src/conponents/ChatApp.jsx
@@ -0,0 +1,128 @@
+import { initializeApp } from "firebase/app";
+import { addDoc, getFirestore, serverTimestamp } from "firebase/firestore";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { collection, query, orderBy, limit } from "firebase/firestore";
+import { useRef, useState } from "react";
+
+import "../App.css";
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqets2wa5qliHWXxKe0Vo2lCclkq3n7-U",
+  authDomain: "chattingapp-filip.firebaseapp.com",
+  projectId: "chattingapp-filip",
+  storageBucket: "chattingapp-filip.appspot.com",
+  messagingSenderId: "334026627618",
+  appId: "1:334026627618:web:e8fb1790b33b808c31887b",
+  measurementId: "G-51P0TGJWCZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+function ChatApp() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => signOut(auth)}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+
+  const messagesRef = collection(firestore, "messages");
+  const messagesQuery = query(messagesRef, orderBy("createdAt"), limit(25));
+
+  const [messages] = useCollectionData(messagesQuery, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    if (!uid) {
+      console.error("User is not authenticated");
+      return;
+    }
+
+    try {
+      await addDoc(messagesRef, {
+        text: formValue,
+        createdAt: serverTimestamp(),
+        uid,
+        photoURL,
+      });
+      setFormValue("");
+      dummy.current.scrollIntoView({ behavior: "smooth" });
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <div ref={dummy}></div>
+      </div>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+
+      <div>
+        <SignOut />
+      </div>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt="profile" />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default ChatApp;
